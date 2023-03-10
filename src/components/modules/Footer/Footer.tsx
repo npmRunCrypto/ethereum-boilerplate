25070381e063ca5365517404d1a300ea3e52c0ca
stark-eth <1319114370@qq.com>
2023-03-10 23:09:46 +0800

    add env config

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -11,13 +11,6 @@ const Footer = () => {
   return (
     <Box textAlign={'center'} w="full" p={6}>
       <Text>
-        ⭐️ Please star this{' '}
-        <Link href={links.github} isExternal alignItems={'center'}>
-          boilerplate <ExternalLinkIcon />
-        </Link>
-        , every star makes us very happy!
-      </Text>
-      <Text>
         🙋 You have questions? Ask them on the{' '}
         <Link href={links.forum} isExternal alignItems={'center'}>
           Moralis forum <ExternalLinkIcon />
